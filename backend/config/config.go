28b7dd6191ee12ce8b4338428095b032f0a1740e
haikali3 <manfdvcl9@gmail.com>
2026-02-25 11:20:40 +0800

    Add configuration and build scripts for project setup

--- backend/config/config.go
+++ backend/config/config.go
@@ -10,7 +10,7 @@ type Config struct {
 	Port         string `env:"SHRINKLY_PORT, default=8080"`
 	DatabaseURL  string `env:"SHRINKLY_DATABASE_URL, required"`
 	MaxWorkers   int    `env:"SHRINKLY_MAX_WORKERS, default=4"`
-	CRF          int    `env:"SHRINKLY_CRF, default=23"`
+	CRF          int    `env:"SHRINKLY_CRF, default=26"`
 	Preset       string `env:"SHRINKLY_PRESET, default=medium"`
 	AudioBitrate string `env:"SHRINKLY_AUDIO_BITRATE, default=128k"`
 	InputDir     string `env:"SHRINKLY_INPUT_DIR, default=./input"`
