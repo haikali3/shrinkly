270f0ecb47d895aa27ac68973c561c29f5d83277
haikali3 <manfdvcl9@gmail.com>
2026-02-25 20:07:16 +0800

    Implement concurrent addition of numbers and create a worker pool structure

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,34 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func addByTwoAtIndex(idx int, val int) (int, int) {
+	val = val + 2
+	return idx, val
+}
+
+func addSliceofNumberbyTwo(nums []int) []int {
+	result := make([]int, len(nums))
+	var wg sync.WaitGroup
+
+	for i, num := range nums {
+		wg.Add(1)
+		go func(idx, val int) {
+			defer wg.Done()
+			outIdx, outVal := addByTwoAtIndex(idx, val)
+			result[outIdx] = outVal
+		}(i, num)
+	}
+	wg.Wait()
+	return result
+}
+
+func main() {
+	sliceOfNumber := []int{1, 2, 3, 4, 5} // dynamic length of array
+	updated := addSliceofNumberbyTwo(sliceOfNumber)
+	fmt.Println("original slice:", sliceOfNumber)
+	fmt.Println("Updated slice:", updated)
+}
--- backend/internal/worker/pool.go
+++ backend/internal/worker/pool.go
@@ -1,1 +1,15 @@
 package worker
+
+type Pool struct {
+	workerCount int
+	maxWorkers  int
+}
+
+func NewPool(workerCount, maxWorkers int) *Pool {
+	return &Pool{
+		workerCount: workerCount,
+		maxWorkers:  maxWorkers,
+	}
+}
+
+func Process()
