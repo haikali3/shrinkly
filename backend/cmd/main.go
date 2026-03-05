2021ba69ebdae23d93fde1151c6e06e66979ba94
haikali3 <manfdvcl9@gmail.com>
2026-03-06 06:26:03 +0800

    fix: update database connection to use pgxpool for improved connection management

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -11,7 +11,7 @@ import (
 	"shrinkly/backend/internal/logger"
 	"shrinkly/backend/internal/worker"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 )
@@ -33,16 +33,24 @@ func main() {
 	defer logger.Sync()
 
 	// connect to db
-	conn, err := pgx.Connect(context.Background(), cfg.DatabaseURL)
+
+	// conn, err := pgx.Connect(context.Background(), cfg.DatabaseURL)
+	// if err != nil {
+	// 	logger.Get().Fatal("failed to connect to database", zap.Error(err))
+	// }
+	// defer conn.Close(context.Background())
+	// queries := db.New(conn)
+
+	dbPool, err := pgxpool.New(context.Background(), cfg.DatabaseURL)
 	if err != nil {
-		logger.Get().Fatal("failed to connect to database", zap.Error(err))
+		logger.Get().Fatal("failed to create database pool", zap.Error(err))
 	}
-	defer conn.Close(context.Background())
-	queries := db.New(conn)
+	defer dbPool.Close()
+	queries := db.New(dbPool)
 
 	// init worker pool + job manager
-	pool := worker.NewPool(cfg.MaxWorkers)
-	manager := job.NewManager(queries, pool, cfg)
+	workerPool := worker.NewPool(cfg.MaxWorkers)
+	manager := job.NewManager(queries, workerPool, cfg)
 
 	// wire router + start http server
 	handler := api.NewHandler(manager, cfg)
